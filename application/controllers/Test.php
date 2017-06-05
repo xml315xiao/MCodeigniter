ef5914d9b7bcd24d79b90a42ee446ae462523e9e
YuanLong <keweiyang315@gamil.com>
2017-06-05 21:49:45 +0800

    add MY_model

--- application/controllers/Test.php
+++ application/controllers/Test.php
@@ -25,7 +25,7 @@ class Test extends CI_Controller
          *
          */
         $this->load->helper('util');
-//        header('content-type:text/plain;charset=utf-8');
+        header('content-type:text/plain;charset=utf-8');
 //        print_r(curl_request('http://www.baidu.com'));
         $this->load->library('curl');
 //        print_r($this->curl->simple_get('http://www.baidu.com'));
@@ -38,6 +38,27 @@ class Test extends CI_Controller
 //        $result = create_captcha();
 //        echo '<img src="'. $result['image']. '">';
 
+
+
+        $this->load->model('Customer_model');
+//        $this->Customer_model->test();
+        $res = $this->Customer_model->get(1001);
+        print_r($this->db->last_query());
+        print_r($res->contact_name);
+        $res = $this->Customer_model->get_by('contact_name', '刘小姐');
+        print_r($this->db->last_query());
+        print_r($res);
+        $res = $this->Customer_model->get_many_by('contact_name', '刘小姐');
+        print_r($this->db->last_query());
+        print_r($res);
+        $res = $this->Customer_model->get_by(['customer_id'=>'1002']);
+        print_r($res);
+        $res = $this->Customer_model->get_row('contact_name', ['customer_id'=>1001]);
+        print_r($res);
+//        $res = $this->c
+
+
+//        print_r($res);
         /*
          * ----------------------------------------
          *
---
+++ application/models/Customer_model.php
@@ -0,0 +1,11 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Customer_model extends MY_Model
+{
+    protected $_table = '';
+    protected $primary_key = 'customer_id';
+
+
+
+}
